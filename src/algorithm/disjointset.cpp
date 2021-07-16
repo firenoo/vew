f4c03d16f1fd3cea63e361bc2e063c09b469d579
firenoo <firenoo57@gmail.com>
2021-07-15 22:11:35 -0700

    Add disjointset implementation, and WC.4

---
+++ src/algorithm/disjointset.cpp
@@ -0,0 +1,71 @@
+#include "disjointset.hpp"
+#include <utility>
+namespace firenoo {
+
+DisjointSet::DisjointSet(size_t size) {
+	_set.reserve(size);
+	for(unsigned int i = 0; i < size; ++i) {
+		_set.emplace_back(i, 0);
+	}
+}
+
+DisjointSet::DisjointSet(DisjointSet&& other) {
+	_set.insert(_set.end(), 
+	            std::make_move_iterator(other._set.begin()),
+				std::make_move_iterator(other._set.end()));
+	other._set.erase(other._set.begin(), other._set.end());
+}
+
+size_t DisjointSet::find(size_t x) {
+	size_t parent = _set[x]._parent;
+	for(; parent != x; parent = _set[x]._parent) {
+		x = _set[x]._parent;
+	}
+	return x;
+}
+
+bool DisjointSet::merge(size_t a, size_t b) {
+	size_t x = find(a);
+	size_t y = find(b);
+	if(x == y) {
+		return false;
+	}
+
+	if(_set[x]._rank < _set[y]._rank) {
+		std::swap(x, y);
+	}
+	_set[y]._parent = x;
+
+	if(_set[x]._rank ==_set[y]._rank) {
+		++_set[x]._rank;
+	}
+	return true;
+}
+
+size_t DisjointSet::max_size() {
+	return _set.size();
+}
+
+
+DisjointSet::Node::Node(size_t parent, size_t rank) :
+	_parent(parent),
+	_rank(rank) {}
+
+DisjointSet::Node::Node(Node&& other) :  _parent(other._parent), _rank(other._rank) {}
+
+DisjointSet::Node& DisjointSet::Node::operator=(Node&& other) noexcept {
+	if(this != &other) {
+		_parent = other._parent;
+		_rank = other._rank;
+	}
+	return *this;
+}
+
+DisjointSet::Node& DisjointSet::Node::operator=(const Node& other) noexcept {
+	if(this != &other) {
+		_parent = other._parent;
+		_rank = other._rank;
+	}
+	return *this;
+}
+}
---
+++ src/algorithm/disjointset.hpp
@@ -0,0 +1,59 @@
+#ifndef _FN_DISJOINTSET
+	#define _FN_DISJOINTSET 0
+	#include <vector>
+	#include <unordered_map>
+
+namespace firenoo {
+
+/*
+ * DisjointSet
+ */
+class DisjointSet {
+
+public:
+	struct Node {
+		//parent index
+		size_t _parent;
+		//rank (max height of node)
+		size_t _rank;
+
+		//Default constructor
+		Node(size_t parent, size_t rank);
+
+		//Move constructor
+		Node(Node&& other);
+
+		//Move assignment
+		Node& operator=(Node&& other) noexcept;
+		//Copy assignment
+		Node& operator=(const Node& other) noexcept;
+	};
+
+	//Backing array
+	std::vector<Node> _set;
+
+	//Default constructor, creates a disjoint set of size `size`.
+	DisjointSet(size_t size) noexcept;
+
+	//Move constructor
+	DisjointSet(DisjointSet&& other) noexcept;
+
+	//Finds the observer (root node) of the specified node.
+	//Input is the index of the node.
+	size_t find(size_t x);
+
+	//Merges the sets that `a` and `b` belong to. Returns
+	//true if the merge occurred
+	bool merge(size_t a, size_t b);
+
+	//gets the size of the backing array.
+	size_t max_size();
+
+	Node& operator[](size_t index) noexcept;
+};
+
+}
+
+
+
+#endif
